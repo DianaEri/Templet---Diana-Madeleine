ffcca1908d54ae2ff154519cc2e437d1c0332874
DianaEri <153087653+DianaEri@users.noreply.github.com>
2023-12-27 15:51:18 +0000

    Creating new slideshow
    Rebuilt the slideshow completely from scratch.

--- script.js
+++ script.js
@@ -1,35 +1,31 @@
-/* Slidern START Jag har använt mig avW3 Schools och Chat Gpt för att finjustera
-W3 Schools: https://www.w3schools.com/howto/howto_js_slideshow.asp
-Chat Gpt: https://chat.openai.com/  */ 
 let slideIndex = 1;
 showSlides(slideIndex);
 
-// Nästa/Föregående kontroller
+// Next/previous controls
 function plusSlides(n) {
-    showSlides(slideIndex += n);
+  showSlides(slideIndex += n);
 }
 
-// Bildkontroller
+// Thumbnail image controls
 function currentSlide(n) {
-    showSlides(slideIndex = n);
+  showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("slides");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace("active");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
-/* Slidern slutar här */
 
 const h2 = document.querySelector('.second_h2')
 
