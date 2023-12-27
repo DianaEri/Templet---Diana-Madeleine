848c66d7d301dbd9d2c59701c9a474a122d19631
DianaEri <153087653+DianaEri@users.noreply.github.com>
2023-12-27 18:59:24 +0000

    Finishing up color themes

--- script.js
+++ script.js
@@ -11,6 +11,28 @@ function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
+// function to set a given theme/color-scheme
+function setTheme(themeName) {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
+}
+// function to toggle between light and dark theme
+function toggleTheme() {
+   if (localStorage.getItem('theme') === 'theme-dark'){
+       setTheme('theme-light');
+   } else {
+       setTheme('theme-dark');
+   }
+}
+// Immediately invoked function to set the theme on initial load
+(function () {
+   if (localStorage.getItem('theme') === 'theme-dark') {
+       setTheme('theme-dark');
+   } else {
+       setTheme('theme-light');
+   }
+})();
+
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
