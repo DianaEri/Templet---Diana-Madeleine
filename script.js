1feabf1ccfc782bd677962a5169185ac307c0f06
DianaEri <153087653+DianaEri@users.noreply.github.com>
2024-01-01 18:25:29 +0000

    Change

--- script.js
+++ script.js
@@ -76,3 +76,11 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+const clearInput = () => {
+  const input = document.getElementsByTagName("input")[0];
+  input.value = "";
+}
+
+const clearBtn = document.getElementById("clear-btn");
+clearBtn.addEventListener("click", clearInput);
